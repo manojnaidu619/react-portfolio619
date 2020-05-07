f7f01acbe2ea99766d4b0a7aff8617a14ef10e58
Manoj Naidu <manojnaidu619@gmail.com>
2020-05-07 17:19:44 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './card'
+
+const App = () => {
+    return (
+        <div>
+            <Card/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/card.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Card = () => {
+    return("From card")
+}
+
+export default Card
