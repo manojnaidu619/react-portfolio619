e6318c3d420254c267f740049b57c9b9dc4d4cec
Manoj Naidu <manojnaidu619@gmail.com>
2020-05-10 16:52:02 +0530

    styled front-card

--- src/components/utils/SocialLinks.js
+++ src/components/utils/SocialLinks.js
@@ -6,7 +6,7 @@ const SocialLinks = () => {
         <div className="social-links">
             <a className="gh-logo" alt="github" href="https://github.com/manojnaidu619" target="_blank" rel="noopener noreferrer"></a>
             <a className="rbgem-logo" alt="rubygems" href="https://rubygems.org/profiles/manojnaidu619" target="_blank" rel="noopener noreferrer"></a>
-            <a className="cv-logo" alt="resume" href="https://drive.google.com/file/d/1xepoKGa9Js7pQVloLtCCBlnea6BKwdte/view" target="_blank" rel="noopener noreferrer"></a>
+            <a className="cv-link" alt="resume" href="https://drive.google.com/file/d/1xepoKGa9Js7pQVloLtCCBlnea6BKwdte/view" target="_blank" rel="noopener noreferrer">RESUME</a>
             <a className="in-logo" alt="linkedin" href="https://www.linkedin.com/in/manoj-kumar-d-b65394159/" target="_blank" rel="noopener noreferrer"></a>
             <a className="dev-logo" alt="dev.to" href="https://dev.to/manojnaidu619" target="_blank" rel="noopener noreferrer"></a>
         </div>
